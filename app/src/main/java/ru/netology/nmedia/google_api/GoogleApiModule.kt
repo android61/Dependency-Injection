23db5c005be6882148aaa0fe8c3fe4972194ec29
Android61 <androidalek@yandex.ru>
2024-07-12 16:28:36 +0300

    first commit DI

---
+++ app/src/main/java/ru/netology/nmedia/google_api/GoogleApiModule.kt
@@ -0,0 +1,17 @@
+package ru.netology.nmedia.google_api
+
+import com.google.android.gms.common.GoogleApiAvailability
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+class GoogleApiModule {
+
+    @Singleton
+    @Provides
+    fun provideGoogleApiAvailability(): GoogleApiAvailability = GoogleApiAvailability.getInstance()
+}
